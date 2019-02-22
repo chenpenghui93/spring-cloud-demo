7fad14ace0374e38e812b419c0a5c18e28b25990
chenpenghui <chenpenghui@hisense.com>
2019-02-22 17:37:57 +0800

    add rabbitmq

--- config-client/src/main/java/com/forezp/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/forezp/configclient/ConfigClientApplication.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,12 @@ import java.security.PublicKey;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class ConfigClientApplication {
+
+    // 不重启服务即能达到配置文件的更新——http://localhost:8881/actuator/bus-refresh
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
