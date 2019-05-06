0ba4af44e89551737a362437bd150d181b14471a
chenpenghui <chenpenghui@hisense.com>
2019-05-06 18:32:17 +0800

    优化

--- service-ribbon/src/main/java/com/forezp/serviceribbon/HelloService.java
+++ service-ribbon/src/main/java/com/forezp/serviceribbon/HelloService.java
@@ -14,7 +14,6 @@ public class HelloService {
     @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
-
     }
 
     public String hiError(String name) {
