838807741356d610debad1cbde92bae13e63efcb
chenpenghui <chenpenghui93@hotmail.com>
2019-05-12 19:15:34 +0800

    断路器监控(Hystrix Dashboard)

--- service-hi/src/main/java/com/forezp/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/forezp/servicehi/ServiceHiApplication.java
@@ -2,34 +2,55 @@ package com.forezp.servicehi;
 
 import brave.sampler.Sampler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * @EnableHystrix 开启断路器
+ * @EnableHystrixDashboard 开启监控面板
+ */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServiceHiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceHiApplication.class, args);
     }
 
-//    @Value("${server.port}")
-//    String port;
-//
-//    @RequestMapping("/hi")
-//    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
-//        return "hi " + name + " ,i am from port:" + port;
-//    }
+    //demo
+    //断路器监控面板 http://localhost:8762/actuator/hystrix.stream
+    @Value("${server.port}")
+    String port;
 
-    private static final Logger logger = Logger.getLogger(ServiceHiApplication.class.getName());
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
+
+    //服务链路追踪示例
+/*    private static final Logger logger = Logger.getLogger(ServiceHiApplication.class.getName());
 
     @Autowired
     private RestTemplate restTemplate;
@@ -54,6 +75,6 @@ public class ServiceHiApplication {
     @Bean
     public Sampler defaultSampler(){
         return Sampler.ALWAYS_SAMPLE;
-    }
+    }*/
 
 }
