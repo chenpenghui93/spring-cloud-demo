b334d12f8e283f36ae5ff434f311de11b67d6883
chenpenghui <chenpenghui@hisense.com>
2019-05-12 16:15:51 +0800

    服务链路追踪 zipkin（已集成在sleuth中）

--- service-miya/src/main/java/com/forezp/servicemiya/ServiceMiyaApplication.java
+++ service-miya/src/main/java/com/forezp/servicemiya/ServiceMiyaApplication.java
@@ -1,4 +1,4 @@
-package com.forezp.servicehi;
+package com.forezp.servicemiya;
 
 import brave.sampler.Sampler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,27 +9,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-public class ServiceHiApplication {
+public class ServiceMiyaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceHiApplication.class, args);
+        SpringApplication.run(ServiceMiyaApplication.class, args);
     }
 
-//    @Value("${server.port}")
-//    String port;
-//
-//    @RequestMapping("/hi")
-//    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
-//        return "hi " + name + " ,i am from port:" + port;
-//    }
-
-    private static final Logger logger = Logger.getLogger(ServiceHiApplication.class.getName());
+    private static final Logger logger = Logger.getLogger(ServiceMiyaApplication.class.getName());
 
     @Autowired
     private RestTemplate restTemplate;
@@ -41,14 +34,14 @@ public class ServiceHiApplication {
 
     @RequestMapping("/hi")
     public String callHome(){
-        logger.log(Level.INFO, "service-hi: calling http://localhost:8989/miya");
-        return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+        logger.log(Level.INFO, "service-miya: hi is being called");
+        return "hi i'm miya!";
     }
 
-    @RequestMapping("/info")
+    @RequestMapping("/miya")
     public String info(){
-        logger.log(Level.INFO, "service-hi: info being called");
-        return "I'm service-hi";
+        logger.log(Level.INFO, "service-miya: calling http://localhost:8988/info");
+        return restTemplate.getForObject("http://localhost:8988/info", String.class);
     }
 
     @Bean
